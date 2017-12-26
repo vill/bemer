f0953274f7db2035ab96948f0213c2a28acc995e
Alexander Grigorev <vill@rubyinventory.org>
2017-12-26 16:26:39 +0700

    Add Configuration class

--- lib/bemer.rb
+++ lib/bemer.rb
@@ -1,7 +1,36 @@
 # frozen_string_literal: true
 
-require 'bemer/version'
+require 'forwardable'
+require 'active_support/dependencies/autoload'
 
 module Bemer
-  # Your code goes here...
+  extend ActiveSupport::Autoload
+
+  autoload :Configuration
+
+  class << self
+    extend Forwardable
+
+    # rubocop:disable Layout/AlignParameters
+    def_delegators :config, :bem,
+                            :default_block_tag,
+                            :default_context,
+                            :default_element_tag,
+                            :element_separator,
+                            :modifier_name_separator,
+                            :modifier_value_separator,
+                            :path,
+                            :transform_string_values
+    # rubocop:enable Layout/AlignParameters
+
+    def config
+      Configuration.instance
+    end
+
+    def setup
+      yield config
+    end
+  end
 end
+
+require 'bemer/version'
---
+++ lib/bemer/configuration.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'singleton'
+
+module Bemer
+  class Configuration
+    include Singleton
+
+    attr_writer :path
+
+    attr_accessor :bem, :default_block_tag, :default_element_tag,
+                  :default_context, :element_separator, :modifier_name_separator,
+                  :modifier_value_separator, :transform_string_values
+
+    def initialize
+      @bem                      = true
+      @default_block_tag        = :div
+      @default_context          = proc {}
+      @default_element_tag      = :div
+      @element_separator        = element_file_separator
+      @modifier_name_separator  = modifier_file_separator
+      @modifier_value_separator = modifier_value_file_separator
+      @transform_string_values  = false
+    end
+
+    def element_file_separator
+      '__'
+    end
+
+    def modifier_file_separator
+      '_'
+    end
+
+    def modifier_value_file_separator
+      modifier_file_separator
+    end
+
+    def path
+      Rails.root.join(@path)
+    end
+  end
+end
