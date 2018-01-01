f54ff53613883240589d0b3a029ef7abb852fab4
Alexander Grigorev <vill@rubyinventory.org>
2018-01-01 22:02:28 +0700

    Rename instance variables

--- lib/bemer/builders/element.rb
+++ lib/bemer/builders/element.rb
@@ -5,14 +5,14 @@ module Bemer
     class Element < Base
       attr_reader :block
 
-      def initialize(name, template, block, **options)
-        super(name, template, options)
+      def initialize(name, view, block, **options)
+        super(name, view, options)
 
         @block = block
       end
 
       def render(content = nil, &callback)
-        Tags::Element.new(block.name, name, template, self, options).render(content, &callback)
+        Tags::Element.new(block.name, name, view, self, options).render(content, &callback)
       end
     end
   end
