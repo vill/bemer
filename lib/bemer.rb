3865f4db4dbeac85b86eec6910a6e2973a4ad8d6
Alexander Grigorev <vill@rubyinventory.org>
2018-01-07 19:49:14 +0700

    Add methods for creating css classes

--- lib/bemer.rb
+++ lib/bemer.rb
@@ -3,6 +3,7 @@
 require 'forwardable'
 require 'active_support/dependencies/autoload'
 require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/string/inflections'
 
 module Bemer
   extend ActiveSupport::Autoload
@@ -39,6 +40,44 @@ module Bemer
 
       Bemer::Tags.eager_load!
       Bemer::Builders.eager_load!
+    end
+
+    def bem_class(block_name, element_name = nil, modifier = nil)
+      modifier_name, modifier_value = *[*modifier, true].flatten
+      modifier_css_class            = modifier_css_class(block_name, element_name,
+                                                         modifier_name, modifier_value)
+
+      modifier_css_class.blank? ? entity_css_class(block_name, element_name) : modifier_css_class
+    end
+
+    def modifier_css_class(block_name, element_name, modifier_name, modifier_value = true)
+      entity_css_class = entity_css_class(block_name, element_name)
+      modifier_name    = nil if modifier_value.blank?
+
+      return '' if entity_css_class.blank? || modifier_name.blank?
+
+      modifier_value = nil if modifier_value.instance_of?(TrueClass)
+      modifier       = css_class(modifier_name, modifier_value,
+                                 separator: modifier_value_separator)
+
+      [entity_css_class, modifier].join(modifier_name_separator)
+    end
+
+    def entity_css_class(block_name, element_name = nil)
+      return '' if block_name.blank?
+
+      [css_class(block_name), css_class(element_name)].reject(&:blank?)
+                                                      .join(element_name_separator)
+    end
+
+    def css_class(*parts, separator: '-')
+      parts.flatten.reject(&:blank?).map do |part|
+        part_without_whitespace = part.to_s.tr(' ', '')
+
+        next part_without_whitespace if !transform_string_values && part.instance_of?(String)
+
+        part_without_whitespace.underscore.dasherize
+      end.join(separator)
     end
   end
 end
---
+++ spec/bemer_spec.rb
@@ -0,0 +1,199 @@
+# frozen_string_literal: true
+
+RSpec.describe Bemer do
+  let(:empty_arguments)           { [nil, false, '', ' '] }
+  let(:empty_class)               { '' }
+  let(:block_name)                { 'Block_Name' }
+  let(:element_name)              { 'Element_Name' }
+  let(:modifier_name)             { 'Has_Tail' }
+  let(:modifier_value)            { 'Yes' }
+
+  shared_context 'when transform_string_values is set to true' do
+    before do
+      described_class.config.transform_string_values = true
+    end
+
+    let(:block_class)               { 'block-name' }
+    let(:element_class)             { 'block-name__element-name' }
+    let(:modifier_class)            { 'block-name__element-name_has-tail' }
+    let(:modifier_class_with_value) { 'block-name__element-name_has-tail_yes' }
+  end
+
+  shared_context 'when transform_string_values is set to false' do
+    before do
+      described_class.config.transform_string_values = false
+    end
+
+    let(:block_class)               { 'Block_Name' }
+    let(:element_class)             { 'Block_Name__Element_Name' }
+    let(:modifier_class)            { 'Block_Name__Element_Name_Has_Tail' }
+    let(:modifier_class_with_value) { 'Block_Name__Element_Name_Has_Tail_Yes' }
+  end
+
+  shared_examples 'bem_class' do
+    it 'creates a block class' do
+      expect(bem_class.call(block_name)).to eq block_class
+    end
+
+    it 'creates an element class', :aggregate_failures do # rubocop:disable RSpec/ExampleLength
+      empty_arguments.each do |arg|
+        expect(bem_class.call(block_name, element_name, arg)).to eq element_class
+        expect(bem_class.call(block_name, element_name, arg => true)).to eq element_class
+        expect(bem_class.call(block_name, element_name, modifier_name => arg)).to eq element_class
+        expect(bem_class.call(block_name, element_name, [modifier_name, arg])).to eq element_class
+        expect(bem_class.call(block_name, element_name, [arg, true])).to eq element_class
+      end
+    end
+
+    it 'creates a modifier class with only name', :aggregate_failures do
+      expect(bem_class.call(block_name, element_name, modifier_name)).to eq modifier_class
+      expect(bem_class.call(block_name, element_name, modifier_name => true)).to eq modifier_class
+      expect(bem_class.call(block_name, element_name, [modifier_name, true])).to eq modifier_class
+      expect(bem_class.call(block_name, element_name, [modifier_name])).to eq modifier_class
+    end
+
+    it 'creates a modifier class with name and value', :aggregate_failures do
+      expect(bem_class.call(block_name, element_name, [modifier_name, modifier_value])).to eq modifier_class_with_value
+      expect(bem_class.call(block_name, element_name, modifier_name => modifier_value)).to eq modifier_class_with_value
+    end
+  end
+
+  shared_examples 'entity_css_class' do
+    it 'creates a block class' do
+      expect(entity_css_class.call(block_name)).to eq block_class
+    end
+
+    it 'creates an element class' do
+      expect(entity_css_class.call(block_name, element_name)).to eq element_class
+    end
+  end
+
+  shared_examples 'modifier_css_class' do
+    it 'creates a modifier class with only name', :aggregate_failures do
+      expect(modifier_css_class.call(block_name, element_name, modifier_name)).to eq modifier_class
+      expect(modifier_css_class.call(block_name, element_name, modifier_name, true)).to eq modifier_class
+    end
+
+    it 'creates a modifier class with name and value' do
+      expect(modifier_css_class.call(block_name, element_name, modifier_name, modifier_value)).to eq modifier_class_with_value
+    end
+  end
+
+  describe '.config' do
+    it 'returns instance of the Configuration class' do
+      expect(described_class.config).to be Bemer::Configuration.instance
+    end
+  end
+
+  describe '.css_class' do
+    subject(:css_class) { described_class.method(:css_class) }
+
+    it 'creates an empty class from empty arguments', :aggregate_failures do
+      expect(css_class.call).to eq empty_class
+      expect(css_class.call(*empty_arguments)).to eq empty_class
+      expect(css_class.call(*empty_arguments, [*empty_arguments])).to eq empty_class
+    end
+
+    it 'creates a class from arguments with whitespaces' do
+      expect(css_class.call('  removes  ', :' all ', ' white  spaces ')).to eq 'removes-all-whitespaces'
+    end
+
+    context 'when transform_string_values is set to true' do
+      before do
+        described_class.config.transform_string_values = true
+      end
+
+      it 'creates a class from strings and symbols' do
+        expect(css_class.call(:some, :Awesome, 'CSS', 'Class')).to eq 'some-awesome-css-class'
+      end
+
+      it 'creates a class from strings and symbols with custom separator' do
+        expect(css_class.call(:some, :Awesome, 'CSS', 'Class', separator: '_')).to eq 'some_awesome_css_class'
+      end
+
+      it 'creates a class from nested strings and symbols' do
+        expect(css_class.call(:Some, ['Awesome', [:CSS]], ['Class'])).to eq 'some-awesome-css-class'
+      end
+    end
+
+    context 'when transform_string_values is set to false' do
+      before do
+        described_class.config.transform_string_values = false
+      end
+
+      it 'creates a class from strings and symbols' do
+        expect(css_class.call(:some, :Awesome, 'CSS', 'Class')).to eq 'some-awesome-CSS-Class'
+      end
+
+      it 'creates a class from strings and symbols with custom separator' do
+        expect(css_class.call(:some, :Awesome, 'CSS', 'Class', separator: '_')).to eq 'some_awesome_CSS_Class'
+      end
+
+      it 'creates a class from nested strings and symbols' do
+        expect(css_class.call(:Some, ['Awesome', [:CSS]], ['Class'])).to eq 'some-Awesome-css-Class'
+      end
+    end
+  end
+
+  describe '.entity_css_class' do
+    subject(:entity_css_class) { described_class.method(:entity_css_class) }
+
+    it 'creates an empty class from empty arguments', :aggregate_failures do
+      empty_arguments.combination(2).each do |block, element|
+        expect(entity_css_class.call(block, element_name)).to eq empty_class
+        expect(entity_css_class.call(block, element)).to eq empty_class
+      end
+    end
+
+    context 'when transform_string_values is set to true' do
+      include_context  'when transform_string_values is set to true'
+      include_examples 'entity_css_class'
+    end
+
+    context 'when transform_string_values is set to false' do
+      include_context 'when transform_string_values is set to false'
+      include_examples 'entity_css_class'
+    end
+  end
+
+  describe '.modifier_css_class' do
+    subject(:modifier_css_class) { described_class.method(:modifier_css_class) }
+
+    it 'creates an empty class from empty arguments', :aggregate_failures do
+      empty_arguments.combination(3).each do |block, element, modifier|
+        expect(modifier_css_class.call(block, element, modifier)).to eq empty_class
+        expect(modifier_css_class.call(block, element, modifier_name)).to eq empty_class
+        expect(modifier_css_class.call(block_name, element_name, modifier_name, modifier)).to eq empty_class
+      end
+    end
+
+    context 'when transform_string_values is set to true' do
+      include_context  'when transform_string_values is set to true'
+      include_examples 'modifier_css_class'
+    end
+
+    context 'when transform_string_values is set to false' do
+      include_context  'when transform_string_values is set to false'
+      include_examples 'modifier_css_class'
+    end
+  end
+
+  describe '.bem_class' do
+    subject(:bem_class) { described_class.method(:bem_class) }
+
+    it 'creates an empty class from empty arguments', :aggregate_failures do
+      expect(bem_class.call(*empty_arguments[0..2])).to eq empty_class
+      expect(bem_class.call(nil, element_name)).to eq empty_class
+    end
+
+    context 'when transform_string_values is set to true' do
+      include_context  'when transform_string_values is set to true'
+      include_examples 'bem_class'
+    end
+
+    context 'when transform_string_values is set to false' do
+      include_context  'when transform_string_values is set to false'
+      include_examples 'bem_class'
+    end
+  end
+end
