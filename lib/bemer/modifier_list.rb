385e525bfc11f1104ec91bb9904959ca609a5025
Alexander Grigorev <vill@rubyinventory.org>
2018-02-19 12:16:40 +0700

    Add ModifierList class

---
+++ lib/bemer/modifier_list.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'active_support/core_ext/object/blank'
+
+module Bemer
+  class ModifierList # rubocop:disable Style/Documentation
+    def initialize(block, element = nil, mods = nil)
+      @block     = block
+      @element   = element
+      @modifiers = build_modifiers(mods)
+    end
+
+    def to_a
+      modifiers
+    end
+
+    def to_s
+      @css_classes ||= modifiers.join(' ')
+    end
+
+    protected
+
+    attr_reader :block, :element, :modifiers
+
+    def build_modifiers(mods)
+      [*mods].flat_map { |attrs| build_modifier(attrs) }.reject(&:blank?).uniq
+    end
+
+    def build_modifier(mods)
+      return mods.map { |attrs| build_modifier(attrs) } if mods.instance_of?(Hash)
+
+      Bemer.modifier_css_class(block, element, *mods)
+    end
+  end
+end
