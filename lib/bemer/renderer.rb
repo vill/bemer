02ce9bb032aeeaf6b47799dd16e0eccef3ab3dd9
Alexander Grigorev <vill@rubyinventory.org>
2018-02-22 10:31:42 +0700

    Clear Renderer class

--- lib/bemer/renderer.rb
+++ lib/bemer/renderer.rb
@@ -1,36 +1,13 @@
 # frozen_string_literal: true
 
-require 'forwardable'
 require 'action_view/helpers/tag_helper'
-require 'active_support/core_ext/object/blank'
 
 module Bemer
   class Renderer # rubocop:disable Style/Documentation
-    extend  Forwardable
-    include ::ActionView::Helpers::TagHelper
-
     def render(builder)
-      @builder = builder
+      return builder.content if builder.tag.blank?
 
-      return content if tag.blank?
-
-      content_tag(tag, content, class: css_classes, **html_options)
-    end
-
-    protected
-
-    METHOD_NAMES = %i[attrs bem? bem_class cls content js_attrs js_class mix mods tag].freeze
-
-    def_instance_delegators :@builder, *METHOD_NAMES
-
-    def html_options
-      bem? ? { **attrs, **js_attrs } : attrs
-    end
-
-    def css_classes
-      return cls unless bem?
-
-      [bem_class, mix, mods, js_class, cls].reject(&:blank?)
+      content_tag(builder.tag, builder.content, class: builder.cls, **builder.attrs)
     end
   end
 end
