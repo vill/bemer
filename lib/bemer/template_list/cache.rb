fb040ad00f47c87a487fe5a7cd980df54798c048
Alexander Grigorev <vill@rubyinventory.org>
2018-03-01 16:44:33 +0700

    Add TemplateList::Cache class

---
+++ lib/bemer/template_list/cache.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Bemer
+  class TemplateList
+    class Cache
+      def initialize(view)
+        @view = view
+      end
+
+      def enable
+        return unless block_given?
+
+        create_template_catalog!
+
+        yield
+
+        remove_template_catalog!
+
+        nil
+      end
+
+      protected
+
+      attr_reader :view
+
+      def create_template_catalog!
+        return unless view.instance_variable_get(:@bemer_template_catalog).nil?
+
+        view.assign(bemer_template_catalog: TemplateCatalog.new(object_id))
+      end
+
+      def remove_template_catalog!
+        template_catalog = view.instance_variable_get(:@bemer_template_catalog)
+
+        return if template_catalog.nil? || !template_catalog.owner.eql?(object_id)
+
+        view.remove_instance_variable(:@bemer_template_catalog)
+      end
+    end
+  end
+end
