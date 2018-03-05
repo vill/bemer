d62e26c148af52e2e8bbe7bdf3b0533a81bcbf81
Alexander Grigorev <vill@rubyinventory.org>
2018-03-05 08:44:19 +0700

    Add initializer `assets_precompile`

--- lib/bemer/railtie.rb
+++ lib/bemer/railtie.rb
@@ -17,5 +17,24 @@ module Bemer
     initializer 'bemer.prepend_view_path', group: :all, after: :load_config_initializers do
       ActionController::Base.prepend_view_path(Bemer.path)
     end
+
+    initializer 'bemer.assets_precompile', group: :all, after: :load_config_initializers do |app|
+      next if !defined?(::Sprockets) || Bemer.precompile.empty?
+
+      Bemer.loose_app_assets = app.config.assets.precompile.shift
+
+      matcher = proc do |logical_path, filename|
+        filename = app.assets.resolve(logical_path).to_s if filename.nil?
+
+        next if filename.start_with?(Bemer.path.to_s)
+
+        loose_app_assets = Bemer.loose_app_assets
+
+        loose_app_assets.call(logical_path, filename) if loose_app_assets.respond_to?(:call)
+      end
+
+      app.config.assets.precompile.unshift(matcher)
+      app.config.assets.precompile << Bemer.precompile
+    end
   end
 end
