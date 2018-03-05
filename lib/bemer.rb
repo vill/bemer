e6a3261769ffb78add41d2946f42e8b3d2cc5082
Alexander Grigorev <vill@rubyinventory.org>
2018-03-05 08:05:17 +0700

    Add autoload ContextExtentions

--- lib/bemer.rb
+++ lib/bemer.rb
@@ -11,6 +11,7 @@ module Bemer
   autoload :Component
   autoload :Configuration
   autoload :Context
+  autoload :ContextExtentions
   autoload :DefaultTemplateList
   autoload :Entity
   autoload :EntityBuilder
