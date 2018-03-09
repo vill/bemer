3f0531a263ca9c1e0ef0371e4992252de45a9c02
Alexander Grigorev <vill@rubyinventory.org>
2018-03-09 14:10:35 +0700

    Separate single and multi argument methods

--- lib/bemer/builders/template.rb
+++ lib/bemer/builders/template.rb
@@ -5,14 +5,28 @@ require 'active_support/core_ext/hash/deep_merge'
 module Bemer
   module Builders
     class Template
+      MULTI_ARGUMENT_METHODS = [Pipeline::ADD_CLS_MODE, Pipeline::ADD_MIX_MODE,
+                                Pipeline::ADD_MODS_MODE, Pipeline::CLS_MODE,
+                                Pipeline::MIX_MODE, Pipeline::MODS_MODE].freeze
+
+      private_constant :MULTI_ARGUMENT_METHODS
+
       def initialize(templates, **options)
         @options   = options
         @predicate = Bemer::Predicate.new(options)
         @templates = templates
       end
 
-      (Pipeline::MODES - Pipeline::STRUCTURE_RELATED_MODES).each do |mode|
+      (Pipeline::MODES - Pipeline::STRUCTURE_RELATED_MODES - MULTI_ARGUMENT_METHODS).each do |mode|
         define_method(mode) do |body|
+          templates.unshift Bemer::Template.new(mode, body, predicate)
+        end
+      end
+
+      MULTI_ARGUMENT_METHODS.each do |mode|
+        define_method(mode) do |*body|
+          body, = *body if body[0].respond_to?(:call)
+
           templates.unshift Bemer::Template.new(mode, body, predicate)
         end
       end
