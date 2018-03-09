ac42dfbd6d546e39519098c37e0d5d3d6e855e63
Alexander Grigorev <vill@rubyinventory.org>
2018-03-09 14:19:02 +0700

    Add small code changes

--- lib/bemer/common_template.rb
+++ lib/bemer/common_template.rb
@@ -12,8 +12,7 @@ module Bemer
       case mode
       when Pipeline::REPLACE_MODE then replace!(node)
       when Pipeline::CONTENT_MODE then node.add_child_nodes
-      else
-        node.public_send mode.to_s.sub('add_', '')
+      else node.public_send mode.to_s.sub('add_', '')
       end
     end
 
