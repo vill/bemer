52c5522e042a3b0e718b49644f2a94f5b61c5a28
Alexander Grigorev <vill@rubyinventory.org>
2018-03-12 09:43:13 +0700

    Change Constants

--- lib/bemer/pipeline.rb
+++ lib/bemer/pipeline.rb
@@ -24,13 +24,11 @@ module Bemer
     REPLACE_MODE   = :replace
     TAG_MODE       = :tag
 
-    STRUCTURE_RELATED_MODES = [REPLACE_MODE, CONTENT_MODE].freeze
     ADD_MODES               = [ADD_ATTRS_MODE, ADD_CLS_MODE, ADD_MIX_MODE, ADD_MODS_MODE].freeze
-    BEM_RELATED_MODES       = [JS_MODE, MIX_MODE, ADD_MIX_MODE, MODS_MODE, ADD_MODS_MODE].freeze
-    TAG_RELATED_MODES       = [BEM_MODE, *BEM_RELATED_MODES, CLS_MODE,
-                               ADD_CLS_MODE, ATTRS_MODE, ADD_ATTRS_MODE].freeze
-    MODES                   = [REPLACE_MODE, TAG_MODE, BEM_MODE, JS_MODE,
-                               MIX_MODE, MODS_MODE, CLS_MODE, ATTRS_MODE, CONTENT_MODE].freeze
+    STRUCTURE_RELATED_MODES = [REPLACE_MODE, CONTENT_MODE].freeze
+    BEM_RELATED_MODES       = [JS_MODE, MIX_MODE, MODS_MODE].freeze
+    TAG_RELATED_MODES       = [BEM_MODE, *BEM_RELATED_MODES, CLS_MODE, ATTRS_MODE].freeze
+    MODES                   = [REPLACE_MODE, TAG_MODE, *TAG_RELATED_MODES, CONTENT_MODE].freeze
 
     def initialize(template_catalog)
       @handlers         = {}
