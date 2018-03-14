1231f7acad4829b3a4d9d7a82d430c3d9091bbbc
Alexander Grigorev <vill@rubyinventory.org>
2018-03-14 18:57:48 +0700

    Remove `transform_string_values`

--- lib/bemer.rb
+++ lib/bemer.rb
@@ -50,8 +50,7 @@ module Bemer
                             :modifier_value_separator,
                             :path,
                             :precompile,
-                            :prepend_assets_path,
-                            :transform_string_values
+                            :prepend_assets_path
     # rubocop:enable Layout/AlignParameters
 
     alias prepend_assets_path? prepend_assets_path
@@ -87,9 +86,9 @@ module Bemer
     def css_class(name)
       name_without_whitespace = name.to_s.delete(' ')
 
-      return name_without_whitespace if name.instance_of?(String) && !transform_string_values
+      return name_without_whitespace if name.instance_of?(String)
 
-      name_without_whitespace.underscore.dasherize
+      name_without_whitespace.dasherize
     end
   end
 end
--- spec/bemer_spec.rb
+++ spec/bemer_spec.rb
@@ -1,45 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe Bemer do
-  let(:empty_arguments)           { [nil, false, '', ' '] }
-  let(:empty_class)               { '' }
-  let(:block_name)                { 'Block_Name' }
-  let(:element_name)              { 'Element_Name' }
-  let(:modifier_name)             { 'Has_Tail' }
-  let(:modifier_value)            { 'Yes' }
-
-  shared_context 'when transform_string_values is set to true' do
-    before do
-      described_class.config.transform_string_values = true
-    end
-
-    let(:block_class)               { 'block-name' }
-    let(:element_class)             { 'block-name__element-name' }
-    let(:modifier_class)            { 'block-name__element-name_has-tail' }
-    let(:modifier_class_with_value) { 'block-name__element-name_has-tail_yes' }
-  end
-
-  shared_context 'when transform_string_values is set to false' do
-    before do
-      described_class.config.transform_string_values = false
-    end
-
-    let(:block_class)               { 'Block_Name' }
-    let(:element_class)             { 'Block_Name__Element_Name' }
-    let(:modifier_class)            { 'Block_Name__Element_Name_Has_Tail' }
-    let(:modifier_class_with_value) { 'Block_Name__Element_Name_Has_Tail_Yes' }
-  end
-
-  shared_examples 'bem_class' do
-    it 'creates a block class' do
-      expect(bem_class.call(block_name)).to eq block_class
-    end
-
-    it 'creates an element class' do
-      expect(bem_class.call(block_name, element_name)).to eq element_class
-    end
-  end
-
   describe '.config' do
     it 'returns instance of the Configuration class' do
       expect(described_class.config).to be Bemer::Configuration.instance
@@ -49,21 +10,27 @@ RSpec.describe Bemer do
   describe '.bem_class' do
     subject(:bem_class) { described_class.method(:bem_class) }
 
+    let(:empty_arguments)           { [nil, false, '', ' '] }
+    let(:block_name)                { 'Block_Name' }
+    let(:element_name)              { 'Element_Name' }
+    let(:block_class)               { 'Block_Name' }
+    let(:element_class)             { 'Block_Name__Element_Name' }
+    let(:modifier_class)            { 'Block_Name__Element_Name_Has_Tail' }
+    let(:modifier_class_with_value) { 'Block_Name__Element_Name_Has_Tail_Yes' }
+
     it 'creates an empty class from empty arguments', :aggregate_failures do
       empty_arguments.combination(2).each do |block, element|
-        expect(bem_class.call(block, element_name)).to eq empty_class
-        expect(bem_class.call(block, element)).to eq empty_class
+        expect(bem_class.call(block, element_name)).to be_empty
+        expect(bem_class.call(block, element)).to be_empty
       end
     end
 
-    context 'when transform_string_values is set to true' do
-      include_context  'when transform_string_values is set to true'
-      include_examples 'bem_class'
+    it 'creates a block class' do
+      expect(bem_class.call(block_name)).to eq block_class
     end
 
-    context 'when transform_string_values is set to false' do
-      include_context 'when transform_string_values is set to false'
-      include_examples 'bem_class'
+    it 'creates an element class' do
+      expect(bem_class.call(block_name, element_name)).to eq element_class
     end
   end
 end
