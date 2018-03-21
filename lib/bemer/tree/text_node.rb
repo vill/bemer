d32867254712af031a154c73b1b8c9d9073391d4
Alexander Grigorev <vill@rubyinventory.org>
2018-03-21 09:01:06 +0700

    Overridden method `capture_content`

--- lib/bemer/tree/text_node.rb
+++ lib/bemer/tree/text_node.rb
@@ -6,6 +6,14 @@ module Bemer
       def initialize(tree, content = nil, &callback)
         super(tree, tag: false, content: content, &callback)
       end
+
+      protected
+
+      def capture_content
+        return content unless content.respond_to?(:call)
+
+        content.binding.receiver.capture(&content)
+      end
     end
   end
 end
